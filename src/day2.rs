6813cd45585cb833ed3ec868ca2a43bbedd4f383
Erich Healy <ErichRHealy@gmail.com>
2017-12-02 01:06:19 -0800

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,5 @@
 use utils;
+use itertools::Itertools;
 
 pub fn day2(input_file: &str) {
     let input: Vec<Vec<i64>> = utils::filelines(input_file)
@@ -8,6 +9,7 @@ pub fn day2(input_file: &str) {
         .collect();
 
     println!("Day 2 part 1 solution: {}", part1(&input));
+    println!("Day 2 part 2 solution: {}", part2(&input));
 }
 
 /*
@@ -51,6 +53,50 @@ fn parse_row(s: &str) -> Vec<i64> {
         .collect()
 }
 
+/*
+--- Part Two ---
+
+"Great work; looks like we're on the right track after all. Here's a star for your effort."
+However, the program seems a little worried. Can programs be worried?
+
+"Based on what we're seeing, it looks like all the User wanted is some information about the evenly
+divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of
+calculation - most of us specialize in bitwise operations."
+
+It sounds like the goal is to find the only two numbers in each row where one evenly divides the
+other - that is, where the result of the division operation is a whole number. They would like you
+to find those numbers on each line, divide them, and add up each line's result.
+
+For example, given the following spreadsheet:
+
+5 9 2 8
+9 4 7 3
+3 8 6 5
+
+* In the first row, the only two numbers that evenly divide are 8 and 2; the result of this
+  division is 4.
+* In the second row, the two numbers are 9 and 3; the result is 3.
+* In the third row, the result is 2.
+
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
+*/
+
+fn divisible_row(row: &[i64]) -> i64 {
+    row.into_iter().combinations(2).filter_map(|combo| {
+        let high = combo[0].max(combo[1]);
+        let low = combo[0].min(combo[1]);
+        if high % low == 0 {
+            Some(high / low)
+        } else {
+            None
+        }
+    }).next().unwrap()
+}
+
+fn part2(input: &[Vec<i64>]) -> i64 {
+    input.iter().map(|x| divisible_row(x)).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,5 +114,12 @@ mod tests {
             vec![2, 4, 6, 8],
         ];
         assert_eq!(part1(&sheet), 18);
+    }
+
+    #[test]
+    fn test_day2_part2_divisible_row() {
+        assert_eq!(divisible_row(&[5, 9, 2, 8]), 4);
+        assert_eq!(divisible_row(&[9, 4, 7, 3]), 3);
+        assert_eq!(divisible_row(&[3, 8, 6, 5]), 2);
     }
 }
