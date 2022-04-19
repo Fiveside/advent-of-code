29fe5be38cc50b4c92046d16e860f2c1ac9e9020
Erich Healy <ErichRHealy@gmail.com>
2022-04-19 13:04:02 -0700

    day 5 boilerplate

---
+++ src/day5.rs
@@ -0,0 +1,21 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day5)]
+fn generator(input: &str) -> u32 {
+    unimplemented!()
+}
+
+#[aoc(day5, part1)]
+fn part1(input: &u32) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        unimplemented!()
+    }
+}
