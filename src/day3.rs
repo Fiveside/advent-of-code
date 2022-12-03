097a451d941569aab0eef49fb8434b7ebce3f34f
Erich Healy <ErichRHealy@gmail.com>
2022-12-03 15:14:48 -0800

    solve day 3 part 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,8 +1,9 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use std::collections::HashSet;
 
-struct Entry(char);
+struct RucksackItem(char);
 
-impl Entry {
+impl RucksackItem {
     fn priority(&self) -> u32 {
         if self.0.is_ascii_uppercase() {
             27 + (self.0 as u32) - ('A' as u32)
@@ -12,17 +13,34 @@ impl Entry {
     }
 }
 
+struct RucksackCollection(String, String);
+
+impl RucksackCollection {
+    fn from_line_entry(line: &str) -> Self {
+        let len = line.len();
+        let (left, right) = line.split_at(len/2);
+        Self(left.to_owned(), right.to_owned())
+    }
+}
+
 
 #[aoc_generator(day3)]
-fn generator(input: &str) -> u32 {
-    input.lines().
+fn generator(input: &str) -> Vec<RucksackCollection> {
+    input.lines().map(RucksackCollection::from_line_entry).collect()
 }
 
 #[aoc(day3, part1)]
-fn part1(input: &u32) -> u32 {unimplemented!()}
+fn part1(input: &[RucksackCollection]) -> u32 {
+    input.iter().map(|rc| {
+        let left = rc.0.chars().collect::<HashSet<_>>();
+        let right = rc.1.chars().collect::<HashSet<_>>();
+        let common = left.intersection(&right).next().unwrap();
+        RucksackItem(common.to_owned()).priority()
+    }).fold(0, |x, y| x + y)
+}
 
 #[aoc(day3, part2)]
-fn part2(input: &u32) -> u32 {unimplemented!()}
+fn part2(input: &[RucksackCollection]) -> u32 {unimplemented!()}
 
 
 #[cfg(test)]
