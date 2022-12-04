dfcc7310c2bf7901c69c0786fb6f86d907e4084b
Erich Healy <ErichRHealy@gmail.com>
2022-12-03 16:04:33 -0800

    day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,21 +1,18 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 use std::collections::HashSet;
 
-struct RucksackItem(char);
-
-impl RucksackItem {
-    fn priority(&self) -> u32 {
-        if self.0.is_ascii_uppercase() {
-            27 + (self.0 as u32) - ('A' as u32)
-        } else {
-            1 + (self.0 as u32) - ('a' as u32)
-        }
+fn priority(item: char) -> u32 {
+    if item.is_ascii_uppercase() {
+        27 + (item as u32) - ('A' as u32)
+    } else {
+        1 + (item as u32) - ('a' as u32)
     }
 }
 
-struct RucksackCollection(String, String);
+struct RucksackPair(String, String);
 
-impl RucksackCollection {
+impl RucksackPair {
     fn from_line_entry(line: &str) -> Self {
         let len = line.len();
         let (left, right) = line.split_at(len/2);
@@ -23,24 +20,42 @@ impl RucksackCollection {
     }
 }
 
+struct ElfGroup(String, String, String);
 
-#[aoc_generator(day3)]
-fn generator(input: &str) -> Vec<RucksackCollection> {
-    input.lines().map(RucksackCollection::from_line_entry).collect()
+impl ElfGroup {
+    fn find_badge(&self) -> char {
+        let left = self.0.chars().collect::<HashSet<char>>();
+        let center = self.1.chars().collect::<HashSet<char>>();
+        let right = self.2.chars().collect::<HashSet<char>>();
+        *left.intersection(&center).filter(|&entry| right.contains(entry)).next().unwrap()
+    }
+}
+
+
+#[aoc_generator(day3, part1)]
+fn generator_part1(input: &str) -> Vec<RucksackPair> {
+    input.lines().map(RucksackPair::from_line_entry).collect()
+}
+
+#[aoc_generator(day3, part2)]
+fn generator_part2(input: &str) -> Vec<ElfGroup> {
+    input.lines().tuples::<(_, _, _)>().map(|(l1, l2, l3)| ElfGroup(l1.to_owned(), l2.to_owned(), l3.to_owned())).collect()
 }
 
 #[aoc(day3, part1)]
-fn part1(input: &[RucksackCollection]) -> u32 {
+fn part1(input: &[RucksackPair]) -> u32 {
     input.iter().map(|rc| {
         let left = rc.0.chars().collect::<HashSet<_>>();
         let right = rc.1.chars().collect::<HashSet<_>>();
         let common = left.intersection(&right).next().unwrap();
-        RucksackItem(common.to_owned()).priority()
+        priority(common.to_owned())
     }).fold(0, |x, y| x + y)
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &[RucksackCollection]) -> u32 {unimplemented!()}
+fn part2(input: &[ElfGroup]) -> u32 {
+    input.iter().map(|x| priority(x.find_badge())).sum()
+}
 
 
 #[cfg(test)]
@@ -57,7 +72,13 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
     #[test]
     fn test_part1() {
-        let data = generator(INPUT_TEXT);
+        let data = generator_part1(INPUT_TEXT);
         assert_eq!(part1(&data), 157);
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = generator_part2(INPUT_TEXT);
+        assert_eq!(part2(&data), 70);
     }
 }
