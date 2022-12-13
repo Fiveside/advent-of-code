2c39607f3633df26e897a4fa3d7cb0feb01c1802
Erich Healy <ErichRHealy@gmail.com>
2022-12-13 12:52:59 -0800

    boilerplate for day10

---
+++ src/day10.rs
@@ -0,0 +1,170 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day10)]
+fn generator(input: &str) -> usize {
+    unimplemented!()
+}
+
+#[aoc(day10, part1)]
+fn part1(input: &usize) -> usize {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT_TEXT: &str = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+";
+
+    #[test]
+    fn test_part1() {
+        let data = generator(INPUT_TEXT);
+        assert_eq!(part1(&data), 13140);
+    }
+}
