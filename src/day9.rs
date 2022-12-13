f94128f2bef528f68e3de8505f396fe7c13d844b
Erich Healy <ErichRHealy@gmail.com>
2022-12-13 01:10:50 -0800

    day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -61,6 +61,12 @@ impl Position {
     }
 }
 
+impl Default for Position {
+    fn default() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
+
 impl Display for Position {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
@@ -95,6 +101,15 @@ fn new_tail(head: &Position, tail: &Position) -> Position {
     }
 
     let xmove = tail.x - head.x;
+    let ymove = tail.y - head.y;
+    if xmove.abs() == ymove.abs() {
+        // Moved both x and y, this is a diagnoal tug.
+        return Position {
+            x: head.x + xmove.signum(),
+            y: head.y + ymove.signum(),
+        };
+    }
+
     if xmove.abs() >= 2 {
         return Position {
             x: head.x + xmove.signum(),
@@ -102,7 +117,6 @@ fn new_tail(head: &Position, tail: &Position) -> Position {
         };
     }
 
-    let ymove = tail.y - head.y;
     if ymove.abs() >= 2 {
         return Position {
             x: head.x,
@@ -124,11 +138,28 @@ fn part1(input: &[Instruction]) -> usize {
             head = head.move_direction(&instruction.direction);
             tail = new_tail(&head, &tail);
             positions.insert(tail);
-            println!("{} {}", head, tail);
         }
     }
-    for pair in positions.iter() {
-        println!("landed on {}", pair);
+
+    positions.len()
+}
+
+#[aoc(day9, part2)]
+fn part2(input: &[Instruction]) -> usize {
+    let mut positions = HashSet::new();
+    let mut knots = vec![Position::default(); 10];
+    for instruction in input {
+        for _ in 0..instruction.magnitude {
+            // move head
+            knots[0] = knots[0].move_direction(&instruction.direction);
+
+            // move knots
+            for idx in 1..knots.len() {
+                knots[idx] = new_tail(&knots[idx - 1], &knots[idx]);
+            }
+
+            positions.insert(*knots.last().unwrap());
+        }
     }
 
     positions.len()
@@ -147,9 +178,30 @@ D 1
 L 5
 R 2";
 
+    const MORE_INPUT_TEXT: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
     #[test]
-    fn test() {
+    fn test_part1() {
         let data = generator(INPUT_TEXT);
         assert_eq!(part1(&data), 13);
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = generator(INPUT_TEXT);
+        assert_eq!(part2(&data), 1);
+    }
+
+    #[test]
+    fn test_part2_more() {
+        let data = generator(MORE_INPUT_TEXT);
+        assert_eq!(part2(&data), 36);
     }
 }
