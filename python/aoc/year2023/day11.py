c551ea92648e1584db40d1b639d00e4bce4d17aa
Erich Healy <ErichRHealy@gmail.com>
2024-01-05 14:53:38 -0800

    Day 11 part 2

--- python/aoc/year2023/day11.py
+++ python/aoc/year2023/day11.py
@@ -2,6 +2,7 @@ from . import year2023
 from typing import NamedTuple, Self
 from io import StringIO
 import itertools
+from dataclasses import dataclass
 
 day = year2023.day(11)
 
@@ -15,8 +16,25 @@ class Galaxy(NamedTuple):
         return abs(other.x - self.x) + abs(other.y - self.y)
 
 
+@dataclass
+class Universe:
+    expanding_rows: list[int]
+    expanding_cols: list[int]
+
+    # These galaxies are not adjusted for universe expansion
+    galaxies: list[Galaxy]
+
+    def galaxies_with_expansion(self, multiplier=2):
+        galaxies = []
+        for galaxy in self.galaxies:
+            dx = sum(1 for x in self.expanding_cols if x < galaxy.x) * (multiplier - 1)
+            dy = sum(1 for x in self.expanding_rows if x < galaxy.y) * (multiplier - 1)
+            galaxies.append(Galaxy(galaxy.id, galaxy.x + dx, galaxy.y + dy))
+        return galaxies
+
+
 @day.generator
-def generator(input: str) -> list[Galaxy]:
+def generator(input: str) -> Universe:
     # First, iterate over rows and columns to figure out which ones are
     # expanding
     lines = [x.strip() for x in StringIO(input).readlines()]
@@ -33,30 +51,29 @@ def generator(input: str) -> list[Galaxy]:
 
     # Record galaxies
     galaxies = []
-    delta_y = 0
     galaxy_id = 0
     for line_no, line in enumerate(lines):
-        if line_no in expanding_rows:
-            delta_y += 1
-            continue
-        delta_x = 0
         for col_no, cell in enumerate(line):
-            if col_no in expanding_cols:
-                delta_x += 1
-                continue
             if cell == "#":
                 galaxy_id += 1
-                galaxies.append(Galaxy(galaxy_id, col_no + delta_x, line_no + delta_y))
+                galaxies.append(Galaxy(galaxy_id, col_no, line_no))
 
-    return galaxies
+    return Universe(expanding_rows, expanding_cols, galaxies)
 
 
 @day.part1
-def part1(galaxies: list[Galaxy]) -> int:
+def part1(universe: Universe) -> int:
+    galaxies = universe.galaxies_with_expansion(2)
     return sum(x.distance(y) for x, y in itertools.combinations(galaxies, 2))
 
 
-@day.test(374)
+@day.part2
+def part2(universe: Universe) -> int:
+    galaxies = universe.galaxies_with_expansion(1_000_000)
+    return sum(x.distance(y) for x, y in itertools.combinations(galaxies, 2))
+
+
+@day.test(374, 82000210)
 def test():
     return """...#......
 .......#..
