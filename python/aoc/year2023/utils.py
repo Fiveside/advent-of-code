62467500f74a12236dd546da4d8e4a8e39fffad2
Erich Healy <ErichRHealy@gmail.com>
2024-02-02 11:57:17 -0800

    Day 13

---
+++ python/aoc/year2023/utils.py
@@ -0,0 +1,13 @@
+import collections
+import itertools
+
+# Straight from the python itertools documentation
+# https://docs.python.org/3/library/itertools.html
+def sliding_window(iterable, n):
+    "Collect data into overlapping fixed-length chunks or blocks."
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(itertools.islice(it, n - 1), maxlen=n)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
