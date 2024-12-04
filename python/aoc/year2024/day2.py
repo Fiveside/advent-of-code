2789a2ec858c7842b0272e32b34d52e518adf774
Erich Healy <ErichRHealy@gmail.com>
2024-12-04 10:58:49 -0800

    2024 day 2 part 2 pre-rewrite

--- python/aoc/year2024/day2.py
+++ python/aoc/year2024/day2.py
@@ -1,6 +1,8 @@
+from collections.abc import Callable
 from io import StringIO
 import itertools
 import math
+import operator
 from typing import Generator
 from . import year2024
 
@@ -42,7 +44,37 @@ def part1(input: list[list[int]]) -> int:
     return sum(1 if is_safe_report(x) else 0 for x in input)
 
 
-@day.test(part1=2)
+def is_faulted_template(
+    compare: Callable[[int, int], bool], assertion: Callable[[list[int]], bool]
+) -> Callable[[list[int]], bool]:
+    def inner(row: list[int]) -> bool:
+        for idx, (x, y) in enumerate(itertools.pairwise(row)):
+            if compare(x, y):
+                return assertion(row[:idx] + row[idx + 1 :])
+        return True
+
+    return inner
+
+
+is_faulted_increasing = is_faulted_template(operator.ge, is_increasing)
+is_faulted_decreasing = is_faulted_template(operator.le, is_decreasing)
+is_faulted_safe_step = is_faulted_template(
+    lambda x, y: not is_safe_single_step(x, y), is_safe_step
+)
+
+
+def is_faulted_safe_report(row: list[int]) -> bool:
+    if not is_faulted_increasing(row) and not is_faulted_decreasing(row):
+        return False
+    return is_faulted_safe_step(row)
+
+
+@day.part2
+def part2(input: list[list[int]]) -> int:
+    return sum(1 if is_faulted_safe_report(x) else 0 for x in input)
+
+
+@day.test(part1=2, part2=4)
 def test():
     return """7 6 4 2 1
 1 2 7 8 9
